9a183f633af63bac117b6667f2642dd83a65583a
Florian <florian@vetter-de.net>
2024-02-02 20:05:33 +0100

    Initial Commit

---
+++ PalCompanionJSON/PalCompanionJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalCompanionJSONApp.swift
+//  PalCompanionJSON
+//
+//  Created by Florian on 02.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PalCompanionJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
